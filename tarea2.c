00ac7c1b091f5df9f9108cf24144186eb362a1f2
yumenikki1 <88511121+yumenikki1@users.noreply.github.com>
2021-09-01 22:20:01 -0500

    Update tarea2.c

--- tarea2.c
+++ tarea2.c
@@ -1,33 +1,31 @@
 #include <stdio.h>
-
-#define FALSE 0
-#define TRUE 1
 #define LEN 10
-int prime(int i) {
+
+int primo(int i) {
   int d;
   for (d = 2; d < i; d++) {
     if (i % d == 0) {
-      return FALSE;
+      return 0;
     }
   }
-  return TRUE;
+  return 1;
 }
 void binarioinvertido(int a, int m){
-while (a > 0 || m > 0) {
+    while (a > 0 || m > 0) {
     printf("%d", a % 2);
     a = a >> 1;
     m--;
-  }
+    }
 }
 int main() {
-    int k = 12;
-  int i;
+    int k = 10;
+    int i;
     int r=1;
-  for (i = 1; r <= k; i++){
-  if (prime(i) == TRUE){
-       binarioinvertido(i, LEN);
-  printf("\n");
-  r++;
+    for (i = 1; r <= k; i++){
+        if (primo(i) == 1){
+        binarioinvertido(i, LEN);
+        printf("\n");
+        r++;
 }
 }
   return 0;
